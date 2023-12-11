7bf5d6db6762b74480ab8d7b1b469fd82f8c5404
Mahfoos <umahfoosahamed@gmail.com>
2023-12-11 23:53:56 +0530

    added tests

---
+++ src/test/java/com/developeriq/githubmetricsservice/GitHubMetricsServiceIntegrationTest.java
@@ -0,0 +1,56 @@
+package com.developeriq.githubmetricsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GitHubMetricsServiceIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetCommitsCount() {
+        String owner = "mahfoos";
+        String repo = "keploy";
+        ResponseEntity<Integer> response = restTemplate.getForEntity("/metrics/github/" + owner + "/" + repo + "/commits", Integer.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+    }
+
+    @Test
+    public void testGetIssuesCount() {
+        String owner = "mahfoos";
+        String repo = "keploy";
+        ResponseEntity<Integer> response = restTemplate.getForEntity("/metrics/github/" + owner + "/" + repo + "/issues", Integer.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+    }
+
+    @Test
+    public void testGetPrCount() {
+        String user = "mahfoos";
+        ResponseEntity<Integer> response = restTemplate.getForEntity("/metrics/github/search/" + user, Integer.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+    }
+
+    @Test
+    public void testGetIssueParticipantCount() {
+        String user = "mahfoos";
+        ResponseEntity<Integer> response = restTemplate.getForEntity("/metrics/github/search/issue/" + user, Integer.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+    }
+
+}
